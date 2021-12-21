4a40d7d10e95ad4a6a8d2d3769ceb97b84421935
theodore markovic <theodore.markovic@svenskaspel.se>
2021-12-22 00:16:06 +0100

    remove unused import

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 
-import Title from './Title';
 import App from './App';
 test('There should be selling title with neat copy', () => {
   render(<App />);
